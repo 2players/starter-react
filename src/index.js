85d93f349ab9ea22fa70ac8a7eb46ca719ba2d98
m31271n <m31271n@users.noreply.github.com>
2019-08-13 13:39:41 +0800

    refactor: use res alias

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import 'normalize.css'
 import './global.css'
 
-import bg from './assets/bg.jpg'
+import bg from 'res/bg.jpg'
 
 const image = document.createElement('img')
+image.style.width = '100%'
+image.style.height = '100%'
+image.style.objectFit = 'cover'
 image.src = bg
 
 document.body.querySelector('#root').appendChild(image)
