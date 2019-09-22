8fe61f36a28980dd434169c57e38819d9299fd7e
m31271n <m31271n@users.noreply.github.com>
2019-09-22 20:34:15 +0800

    feat: update example code

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './page.css'
 
 function Page() {
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('Julia')
+
+  useEffect(() => {
+    console.log(`Hi ${name}, you clicked ${count} times!`)
+  })
+
+  useEffect(() => {
+    console.log(`Hi ${name}, you clicked ${count} times!`)
+  })
+
   return (
     <>
-      <h1 styleName="title">React Starter</h1>
-      <p styleName="content">Hello World!</p>
+      <p>
+        Hi {name}, You clicked {count} times!
+      </p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setName(name === 'Julia' ? 'Spike' : 'Julia')}>
+        Change Name
+      </button>
     </>
   )
 }
